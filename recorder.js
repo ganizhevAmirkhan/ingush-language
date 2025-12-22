a91735d8f005afaaeb2e6f479c2a7c546a9af6ff
ganizhevAmirkhan <ganizhev.a.a@gmail.com>
2025-12-22 21:41:22 +0300

    Update recorder.js

--- recorder.js
+++ recorder.js
@@ -1,1 +1,10 @@
+async function recordWord(){
+  startRecording("words", current.id);
+}
+
+async function recordExample(){
+  const id = "ex_" + Date.now();
+  current.senses[0].examples[0].audio = id+".mp3";
+  startRecording("examples", id);
+}
 
