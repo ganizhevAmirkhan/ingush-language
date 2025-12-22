64d63f7b32acfb0925b73b30be8e6c1fdca28ac4
ganizhevAmirkhan <ganizhev.a.a@gmail.com>
2025-12-22 21:41:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open("dict-v2").then(c=>
+      c.addAll(["./","index.html","style.css","script.js"])
+    )
+  );
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
 
